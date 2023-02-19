0a93ae40fb510bc937c2092262f0a343c9b88add
Temidayo Oluwagbemiga <esthertemidayo82@gmail.com>
2023-02-19 13:25:28 -0800

    task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,23 @@ int main(void)
 {
 	int p, q;
 
-	for (p = 0; p <= 98; p++)
+	for (p = 0; p < 100; p++)
 	{
-		for (q = p + 1; q <= 99; q++)
+		if (p < q)
 		{
-			putchar((p / 10) + '0');
-			putchar((p % 10) = '0');
-			putchar(' ');
-			putchar((q / 10) + '0');
-			putchar((q % 10) + '0');
-			if (p == 98 && q == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
+			for (q = 0; q < 100; q++)
+			{
+				putchar((p / 10) + 48);
+				putchar((p % 10) + 48);
+				putchar(' ');
+				putchar((q / 10) + 48);
+				putchar((q % 10) + 48);
+				if (p != 98 || q != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
